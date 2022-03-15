6b08e00c49068bad80f9d20e41492ae0a79548a5
lateef <olulat01@gmail.com>
2022-03-15 11:43:53 +0000

    Read functionality

--- src/main/java/com/lateefolu/project/service/ProjectController.java
+++ src/main/java/com/lateefolu/project/service/ProjectController.java
@@ -3,10 +3,9 @@ package com.lateefolu.project.service;
 import com.lateefolu.project.model.Project;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/project")
@@ -21,6 +20,17 @@ public class ProjectController {
     public ResponseEntity<Project> addProject(@RequestBody Project project){
         Project newProject = projectService.addProject(project);
         return new ResponseEntity<>(newProject, HttpStatus.CREATED);
+    }
 
+    @GetMapping("/all")
+    public ResponseEntity<List<Project>> getAllProject(){
+        List<Project> projects = projectService.viewAllProject();
+        return new ResponseEntity<>(projects, HttpStatus.OK);
+    }
+
+    @GetMapping("/find/{projectCode}")
+    public ResponseEntity<Project> getProjectByCode(@PathVariable("projectCode") String projectCode){
+        Project project = projectService.findProjectByProjectCode(projectCode);
+        return new ResponseEntity<>(project, HttpStatus.OK);
     }
 }
--- src/main/java/com/lateefolu/project/service/ProjectService.java
+++ src/main/java/com/lateefolu/project/service/ProjectService.java
@@ -1,13 +1,18 @@
 package com.lateefolu.project.service;
 
+import com.lateefolu.project.exception.ProjectNotFoundException;
 import com.lateefolu.project.model.Project;
 import com.lateefolu.project.repo.ProjectRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ProjectService {
     private final ProjectRepo projectRepo;
 
+    @Autowired
     public ProjectService(ProjectRepo projectRepo) {
         this.projectRepo = projectRepo;
     }
@@ -15,4 +20,15 @@ public class ProjectService {
     public Project addProject(Project NewProject){
         return projectRepo.save(NewProject);
     }
+
+    public List<Project> viewAllProject(){
+        return projectRepo.findAll();
+    }
+
+    public Project findProjectByProjectCode(String projectCode){
+        return projectRepo.findProjectByProjectCode(projectCode).orElseThrow(
+                () ->new  ProjectNotFoundException("Project with " + projectCode + " was not found" ));
+    }
+
+
 }
