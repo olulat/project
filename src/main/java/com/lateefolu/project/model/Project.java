09a1cfd83b9f6dbe9103660a7d89d9bafe4fd528
lateef <olulat01@gmail.com>
2022-03-16 10:34:44 +0000

    Adding swager documentation

--- src/main/java/com/lateefolu/project/model/Project.java
+++ src/main/java/com/lateefolu/project/model/Project.java
@@ -1,8 +1,16 @@
 package com.lateefolu.project.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Project {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -12,68 +20,10 @@ public class Project {
     private String description;
     private String imageUrl;
     @Column(unique = true)
+    @NotEmpty(message = "Project code cannot be empty or null")
     private String projectCode;
 
-    public Project() {
-    }
 
-    public Project(String name, String description, String imageUrl, String projectCode) {
-        this.name = name;
-        this.description = description;
-        this.imageUrl = imageUrl;
-        this.projectCode = projectCode;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getImageUrl() {
-        return imageUrl;
-    }
-
-    public void setImageUrl(String imageUrl) {
-        this.imageUrl = imageUrl;
-    }
-
-    public String getProjectCode() {
-        return projectCode;
-    }
-
-    public void setProjectCode(String projectCode) {
-        this.projectCode = projectCode;
-    }
-
-    @Override
-    public String toString() {
-        return "Project{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", description='" + description + '\'' +
-                ", imageUrl='" + imageUrl + '\'' +
-                ", projectCode='" + projectCode + '\'' +
-                '}';
-    }
 }
 
 
